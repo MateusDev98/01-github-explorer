093596049c35106062e3c4b92a9eb9494ac60178
Mateus Silva <mateus.lopes9812@gmail.com>
2022-09-12 19:13:38 -0300

    Finalização do módulo

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   presets: [
     "@babel/preset-env", // Identifica em qual ambiente a aplicação está sendo executada para converter o código da melhor maneira possível
+    "@babel/preset-typescript", // Faz com que o babel entenda typescript
     ["@babel/preset-react", { runtime: "automatic" }], // A configuração de runtime setada como 'automatic' remove a necessidade de importação do react em cada componente/função
   ],
 };
